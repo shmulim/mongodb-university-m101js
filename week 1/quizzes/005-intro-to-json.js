981fca56cb9a35334819f8e5ff3a696e0a8fc0b7
@shmulim <shmuel.markel@gmail.com>
2015-06-23 14:23:05 -0700

    Create 005-intro-to-json.js

---
+++ week 1/quizzes/005-intro-to-json.js
@@ -0,0 +1,6 @@
+// Which of the following are JSON documents that the MongoDB shell will accept?
+
+// * { a : 1, b : 2, c : 3 }
+// * { a : 1, b : 2, c : [ 1, 2, 3, 4, 5 ] }
+// * { a : 1, b : { }, c : [ { a : 1, b : 2 }, 5, 6 ] }
+// * { }
