291f1d3222fa809763c4d354f197b13576af0226
@shmulim <shmuel.markel@gmail.com>
2015-06-23 14:25:14 -0700

    Update 006-json-revisited.js

--- week 1/quizzes/006-json-revisited.js
+++ week 1/quizzes/006-json-revisited.js
@@ -1,3 +1,4 @@
-// Write the JSON for a simple document containing a single key "fruit" that has as its value an array containing three strings: "apple", "pear", and "peach"
+// Write the JSON for a simple document containing a single key "fruit" that has as its value an array
+// containing three strings: "apple", "pear", and "peach"
 
 // {"fruit": ["apple", "pear", "peach"]}
