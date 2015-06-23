c091a306d4b6590394a52696c34329741099d8ea
@shmulim <shmuel.markel@gmail.com>
2015-06-23 14:41:22 -0700

    Create 010-intro-to-schema-design.js

---
+++ week 1/quizzes/010-intro-to-schema-design.js
@@ -0,0 +1,4 @@
+// In which scenario is it impossible to embed data within a document
+// (you must put the data in it a separate collection)?
+
+// The embedded data could exceed the 16MB document limit within MongoDB.
