7362a926a14c3313ddde9db3f573efef0911e7b8
@shmulim <shmuel.markel@gmail.com>
2015-06-23 14:27:21 -0700

    Create 007-json-subdocuments.js

---
+++ week 1/quizzes/007-json-subdocuments.js
@@ -0,0 +1,11 @@
+// Write a JSON document with a single key, "address" that has as it value another document
+// with the keys “street_address”, “city”, “state”, “zipcode”, with the
+// following values: “street_address” is "23 Elm Drive", “city” is "Palo Alto",
+// “state” is "California", “zipcode” is "94305"
+
+// {"address" : {
+//     "street_address" : "23 Elm Drive",
+//     "city" : "Palo Alto",
+//     "state" : "California",
+//     "zipcode" : "94305"
+// }}
