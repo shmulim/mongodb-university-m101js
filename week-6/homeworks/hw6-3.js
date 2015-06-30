b308eec7719eb12472b5f03e5b233fbe8603bc6f
@shmulim <shmuel.markel@gmail.com>
2015-06-30 15:52:31 -0700

    Create hw6-3.js

---
+++ week-6/homeworks/hw6-3.js
@@ -0,0 +1,6 @@
+// Which of the following statements are true about choosing and using a shard key?
+
+// * MongoDB can not enforce unique indexes on a sharded collection
+other than the shard key itself, or indexes prefixed by the shard key.
+// * Any update that does not contain the shard key will be sent to all shards.
+// * There must be a index on the collection that starts with the shard key.
