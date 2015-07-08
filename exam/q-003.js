811d3ad53ca3427ee256475189e35012740f93d5
@shmulim <shmuel.markel@gmail.com>
2015-07-07 22:37:26 -0700

    Update q-003.js

--- exam/q-003.js
+++ exam/q-003.js
@@ -1,8 +1,9 @@
 // update
-db.getCollection('messages').update({'headers.Message-ID': '<8147308.1075851042335.JavaMail.evans@thyme>'}, {$push: {'headers.To': 'mrpotatohead@mongodb.com'}})
+db.getCollection('messages').update({'headers.Message-ID': '<8147308.1075851042335.JavaMail.evans@thyme>'},
+  {$push: {'headers.To': 'mrpotatohead@mongodb.com'}});
 
 // manually verify
-db.getCollection('messages').find({'headers.Message-ID': '<8147308.1075851042335.JavaMail.evans@thyme>'})
+db.getCollection('messages').find({'headers.Message-ID': '<8147308.1075851042335.JavaMail.evans@thyme>'});
 
 // validation code
 vOnRg05kwcqyEFSve96R
